f21cb718ac2ed39399845940d5b66a87a60c4556
Kanaad Parvate <kanaadp@gmail.com>
2016-02-27 03:50:52 -0800

    started flask app under web-app

---
+++ web-app/schema.sql
@@ -0,0 +1,21 @@
+drop table if exists colleges;
+create table colleges (
+college_id integer primary key autoincrement;
+latitude DOUBLE not null;
+longitude DOUBLE not null;
+name string VARCHAR not null;
+)
+
+drop table if exists raw_yaks;
+create table raw_yaks (
+yak_id integer primary key autoincrement;
+college_id integer not null;
+yak_text VARCHAR not null;
+upvotes integer not null;
+)
+
+drop table if exists most_valuable_words;
+create table most_valuable_words (
+college_id integer primary key not null;
+word_text VARCHAR not null;
+)
